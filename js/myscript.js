7d2c0dd7c600eeb7e288724e6dddedf3ea019d39
Marco-Vigilio <tonyvigilio@gmail.com>
2023-05-17 15:14:01 +0200

    esercizio email

--- js/myscript.js
+++ js/myscript.js
@@ -1,11 +1,15 @@
-
+/*
+Generare un numero random da 1 a 6, 
+sia per il giocatore sia per il computer.
+Stabilire il vincitore, 
+in base a chi fa il punteggio più alto.
+*/
 let num_utente;
 let num_pc;
-const bottone = document.querySelector("input");
+const bottoneUno = document.getElementById("lancio_dadi");
 
-const risultato = document.createElement("p");
 
-bottone.addEventListener("click", function(){
+bottoneUno.addEventListener("click", function(){
 
     num_utente = Math.floor(Math.random() * 7) + 1;
     console.log("Dado utente: " + num_utente);
@@ -25,3 +29,43 @@ bottone.addEventListener("click", function(){
     console.log();
 });
 
+/*
+* Chiedi all’utente la sua email,
+* controlla che sia nella lista di chi può accedere,
+  stampa un messaggio appropriato sull’esito del controllo.
+
+Tenete presente che non è permesso utilizzare funzioni
+ built-in degli array come 
+ Array.includes() e 
+ Array.indexOf(), 
+ pena l'annullamento dell'esercizio.
+ */
+
+const bottoneDue = document.getElementById("invio_email");
+const email = document.getElementById(("user_email"));
+
+const array = ["ciao@ciao", "tony@tony.it", "tony.tony@boolean.it"];
+
+bottoneDue.addEventListener("click", function(){
+    
+    console.log("Email digitata: " + email.value);
+    let accettato;
+    let nonAccetato;
+    let verifica;
+
+    for(let i = 0; i < array.length; i++){
+        console.log("Email accettata: " + array[i]);
+        if(email.value === array[i]){
+            console.log("Accettato");
+            verifica = "Accettato";
+        }
+        else{
+            console.log("Non Autorizzato");
+            verifica = "Non accettato";
+        }
+        console.log();
+    }
+    console.log(verifica);
+    console.log();
+}
+);
